4290e950dba97ab2256910f4d36353acf5762841
Samet <99058368@mydavinci.nl>
2020-09-20 18:29:43 +0200

    Variabelen_JS

---
+++ B1W3L2_Variabelen/JS/variabelen.js
@@ -0,0 +1,17 @@
+//Samet Hira
+//Software Developer
+
+//It starts a function and only succeeds to work if the user has pressed the button associated with the "onclick" line.
+function wieBenIk(){
+
+//Asks the name.
+const NAME = prompt("Naam?");
+
+//Asks the surname.
+const SURNAME = prompt("Achternaam?");
+
+//Asks the age.
+const AGE = prompt("Leeftijd?");
+
+//Displays the results in an H1 format on the website.
+document.write("<h1>" + "Hallo, je heet " + NAME + " " + SURNAME + " en bent " + AGE + " jaar!" + "</h1>");}
